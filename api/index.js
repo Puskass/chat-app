1e3bd1d1263e88761a47be2c47ef52f719bc0c3b
Puskass <cosovictarik711@gmail.com>
2023-07-25 22:18:41 +0200

    some shit not working

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
@@ -18,11 +17,20 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
   socket.on("send_message", (data) => {
-    socket.broadcast.emit("receive_message", data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
   });
 });
 
 server.listen(5000, () => {
-  console.log("Server running on port 5000");
+  console.log("SERVER RUNNING");
 });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import io from "socket.io-client"
+import React, { useState } from "react";
+import io from "socket.io-client";
+import Chats from "./components/Chats";
+import "./App.css"
 
-const socket = io.connect("http://localhost:5000")
- 
+const socket = io.connect("http://localhost:5000");
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
-export default App
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join a chat</h3>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID"
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join a Room</button>
+        </div>
+      ) : (
+        <Chats socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+};
+
+export default App;
